7eb7feaf6553144e6fe4896e15e2e39215f5c3c6
LuisGRueda <ruedafloresluisgabriel@gmail.com>
2024-03-14 21:38:28 -0400

    CRUD completo Institucion Storage Account de azure

---
+++ Coling/Coling.API.Curriculum/Contratos/Repositorios/IInstitucionRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using Coling.API.Curriculum.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coling.API.Curriculum.Contratos.Repositorios
+{
+    public interface IInstitucionRepositorio
+    {
+        public Task<bool> Create(Institucion institucion);
+        public Task<bool> Update(Institucion institucion);
+        public Task<bool> Delete(string partitionKey, string rowKey);
+        public Task<List<Institucion>> GetAll();
+        public Task<Institucion> Get(string id);
+    }
+}
